f0ef8e01419b4b3f9bfb3ca6ca1a81ae70e2c98f
YexingWan <wanyexing@hotmail.com>
2018-12-27 15:38:49 +0800

    modify loss

--- models.py
+++ models.py
@@ -730,7 +730,7 @@ class YOLOLayer(nn.Module):
                 loss_conf = self.bce_loss(pred_conf[conf_mask_false],
                                           tconf[conf_mask_false]) \
                             + self.bce_loss(pred_conf[conf_mask_true],
-                                            tconf[conf_mask_true]) * 5
+                                            tconf[conf_mask_true])
 
                 loss_cls = self.ce_loss(pred_cls[mask], torch.argmax(tcls[mask],1))
 
@@ -740,7 +740,7 @@ class YOLOLayer(nn.Module):
                 loss_y = torch.tensor(0)
                 loss_w = torch.tensor(0)
                 loss_h = torch.tensor(0)
-                loss_conf = self.bce_loss(pred_conf[conf_mask], tconf[conf_mask])*6
+                loss_conf = self.bce_loss(pred_conf[conf_mask], tconf[conf_mask])*2
                 loss_cls = torch.tensor(0)
 
             loss = loss_x + loss_y + loss_w + loss_h + loss_conf + loss_cls
