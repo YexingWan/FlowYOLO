61024da1b554a1e5d98c433c961e0e8c4c235290
YexingWan <wanyexing@hotmail.com>
2019-01-23 11:00:56 +0800

    infer test

--- train.sh
+++ train.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 yolo_config_path='./config/head_yolov3.cfg'
-yolo_resume='./work/checkpoints/10000_weights/yolo_f.pth'
+yolo_resume='./work/checkpoints_head/10000_weights/yolo_f.pth'
 flow_model='FlowNet2CS'
-flow_resume='./work/checkpoints/10000_weights/flow.pth'
+flow_resume='./work/checkpoints_head/10000_weights/flow.pth'
 train_batch_size=3
 data_config_path='./config/head_data.data'
 validation_frequency=1000
